f993f12b9352f0c7a33f0e0b67b6e2dca5f2c8ac
christian0101 <christian139601@gmail.com>
2020-02-11 10:40:01 +0000

    Fix formatting

--- ThroughIFKit.java
+++ ThroughIFKit.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2007 Phidgets Inc.  All rights reserved.
  */
@@ -5,71 +6,70 @@
 import com.phidgets.*;
 import com.phidgets.event.*;
 
-public class ThroughIFKit
-{
-	public static final void main(String args[]) throws Exception {
+public class ThroughIFKit {
+  public static final void main(String args[]) throws Exception {
     InterfaceKitPhidget ik;
     TextLCDPhidget lcd;
 
-		//Example of enabling logging.
-		//Phidget.enableLogging(Phidget.PHIDGET_LOG_VERBOSE, null);
+    // Example of enabling logging.
+    // Phidget.enableLogging(Phidget.PHIDGET_LOG_VERBOSE, null);
 
-		System.out.println(Phidget.getLibraryVersion());
+    System.out.println(Phidget.getLibraryVersion());
 
     lcd = new TextLCDPhidget();
     lcd.addAttachListener(new AttachListener() {
 
-        public void attached(AttachEvent ae) {
-            System.out.println("attachment of " + ae);
-        }
+      public void attached(AttachEvent ae) {
+        System.out.println("attachment of " + ae);
+      }
     });
 
     lcd.addDetachListener(new DetachListener() {
 
-        public void detached(DetachEvent ae) {
-            System.out.println("detachment of " + ae);
-        }
+      public void detached(DetachEvent ae) {
+        System.out.println("detachment of " + ae);
+      }
     });
 
     lcd.addErrorListener(new ErrorListener() {
 
-        public void error(ErrorEvent ee) {
-            System.out.println("error event for " + ee);
-        }
+      public void error(ErrorEvent ee) {
+        System.out.println("error event for " + ee);
+      }
     });
 
     lcd.openAny();
     System.out.println("Waiting for the TextLCD to be attached...");
     lcd.waitForAttachment();
 
-		ik = new InterfaceKitPhidget();
-		ik.addAttachListener(new AttachListener() {
-			public void attached(AttachEvent ae) {
-				System.out.println("attachment of " + ae);
-			}
-		});
-		ik.addDetachListener(new DetachListener() {
-			public void detached(DetachEvent ae) {
-				System.out.println("detachment of " + ae);
-			}
-		});
-		ik.addErrorListener(new ErrorListener() {
-			public void error(ErrorEvent ee) {
-				System.out.println(ee);
-			}
-		});
-		ik.addInputChangeListener(new InputChangeListener() {
-			public void inputChanged(InputChangeEvent oe) {
-				//System.out.println(oe);
-			}
-		});
-		ik.addOutputChangeListener(new OutputChangeListener() {
-			public void outputChanged(OutputChangeEvent oe) {
-				//System.out.println(oe);
-			}
-		});
-		ik.addSensorChangeListener(new SensorChangeListener() {
-			public void sensorChanged(SensorChangeEvent se) {
+    ik = new InterfaceKitPhidget();
+    ik.addAttachListener(new AttachListener() {
+      public void attached(AttachEvent ae) {
+        System.out.println("attachment of " + ae);
+      }
+    });
+    ik.addDetachListener(new DetachListener() {
+      public void detached(DetachEvent ae) {
+        System.out.println("detachment of " + ae);
+      }
+    });
+    ik.addErrorListener(new ErrorListener() {
+      public void error(ErrorEvent ee) {
+        System.out.println(ee);
+      }
+    });
+    ik.addInputChangeListener(new InputChangeListener() {
+      public void inputChanged(InputChangeEvent oe) {
+        // System.out.println(oe);
+      }
+    });
+    ik.addOutputChangeListener(new OutputChangeListener() {
+      public void outputChanged(OutputChangeEvent oe) {
+        // System.out.println(oe);
+      }
+    });
+    ik.addSensorChangeListener(new SensorChangeListener() {
+      public void sensorChanged(SensorChangeEvent se) {
         String strValue = "0";
         String msg = "Index " + se.getIndex() + " Value";
 
@@ -82,24 +82,24 @@ public class ThroughIFKit
           strValue = Double.toString(se.getValue());
         }
 
-        // DEBUG 
+        // DEBUG
         System.out.println("Index " + se.getIndex() + ": " + strValue);
 
         try {
           lcd.setDisplayString(0, msg);
           lcd.setDisplayString(1, strValue);
-        } catch(Exception e) {
+        } catch (Exception e) {
           System.out.println(e);
         }
-			}
-		});
+      }
+    });
 
-		ik.openAny();
-		System.out.println("waiting for InterfaceKit attachment...");
-		ik.waitForAttachment();
+    ik.openAny();
+    System.out.println("waiting for InterfaceKit attachment...");
+    ik.waitForAttachment();
 
-		System.out.println(ik.getDeviceName());
+    System.out.println(ik.getDeviceName());
 
-		Thread.sleep(500);
-	}
+    Thread.sleep(500);
+  }
 }
