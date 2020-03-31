a90947f0801314b1af560135c4596d1c5c1ae4ad
Slobodov Aleksandr <gerons.y@gmail.com>
2020-03-31 15:30:16 +0100

    Working on the UI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    Label welcomeLabel;
+
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        welcomeLabel.setText("Welcome, today is: " + dtf.format(now));
+
+    }
 }
